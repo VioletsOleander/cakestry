2f5d8d85601b92e0e24c1a584bf5977980df0f4c
VioletsOleander <1377232072@qq.com>
2026-08-06 22:25:35 +0800

    refactor: initialize tracing subscriber to main function
    This is supposed to be the app's job instead of the lib's job, lib should only care about emitting
    tracing event.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,14 @@ use cakestry::App;
 struct Args {}
 
 fn main() -> Result<()> {
+    // Initialize the default global tracing subscriber.
+    let appender = tracing_appender::rolling::never(".", "cakestry.log");
+    let subscriber_builder = tracing_subscriber::fmt()
+        .with_ansi(false)
+        .with_writer(appender);
+
+    subscriber_builder.init();
+
     let _ = Args::parse();
     let mut app = App::default();
 
