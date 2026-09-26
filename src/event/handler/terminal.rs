58dfec3428ce3dc84b4627d3cd191576b72b2aeb
Xinjie Chen <1377232072@qq.com>
2026-09-26 15:45:55 +0800

    refactor: implement editing functionality for `TextBuffer`
    PR: https://github.com/VioletsOleander/cakestry/pull/17

--- src/event/handler/terminal.rs
+++ src/event/handler/terminal.rs
@@ -1,25 +1,21 @@
-use crossterm::event::{Event, KeyCode, KeyModifiers};
+use crossterm::event::{Event, KeyCode, KeyModifiers, MouseEventKind};
 
 use crate::event::service::ResponseEventService;
 use crate::state::State;
-
-enum BufferType {
-    Prompt,
-    Command,
-}
+use crate::state::buffer::{TextBuffer, TextBufferKind};
 
 pub struct TerminalEventHandler {
-    resp_service: ResponseEventService,
-    wait_resp: bool,
-    active_buffer: BufferType,
+    service: ResponseEventService,
+    wait_response: bool,
+    active_buffer_kind: TextBufferKind,
 }
 
 impl TerminalEventHandler {
-    pub fn new(resp_service: ResponseEventService) -> Self {
+    pub fn new(service: ResponseEventService) -> Self {
         TerminalEventHandler {
-            resp_service,
-            wait_resp: false,
-            active_buffer: BufferType::Prompt,
+            service,
+            wait_response: false,
+            active_buffer_kind: TextBufferKind::Prompt,
         }
     }
 
@@ -28,47 +24,75 @@ impl TerminalEventHandler {
             Event::Key(key) => match key.modifiers {
                 KeyModifiers::CONTROL => match key.code {
                     // CTRL-Esc: Exit command mode, back to prompt mode.
-                    KeyCode::Esc => match self.active_buffer {
-                        BufferType::Command => self.active_buffer = BufferType::Prompt,
-                        _ => (),
-                    },
+                    KeyCode::Esc => {
+                        if let TextBufferKind::Command = self.active_buffer_kind {
+                            self.active_buffer_kind = TextBufferKind::Prompt
+                        }
+                    }
                     // CTRL-c: Enter command mode.
-                    KeyCode::Char('c') => self.active_buffer = BufferType::Command,
+                    KeyCode::Char('c') => self.active_buffer_kind = TextBufferKind::Command,
+                    // CTRL-j: Break current line.
+                    KeyCode::Char('j') => self.get_active_buffer(state).break_line(),
+                    // CTRL-w: Delete a word backward.
+                    KeyCode::Char('w') => self.get_active_buffer(state).delete_word_backward(),
+                    // CTRL-u: Delete a line backward.
+                    KeyCode::Char('u') => self.get_active_buffer(state).delete_line_backward(),
                     _ => (),
                 },
                 KeyModifiers::SHIFT => match key.code {
                     // SHIFT-Esc: Exit command mode, back to prompt mode.
-                    KeyCode::Esc => match self.active_buffer {
-                        BufferType::Command => self.active_buffer = BufferType::Prompt,
-                        _ => (),
-                    },
+                    KeyCode::Esc => {
+                        if let TextBufferKind::Command = self.active_buffer_kind {
+                            self.active_buffer_kind = TextBufferKind::Prompt
+                        }
+                    }
+                    KeyCode::Char(ch) => self.get_active_buffer(state).insert_char(ch),
                     _ => (),
                 },
                 KeyModifiers::NONE => match key.code {
                     // Esc: Exit command mode, back to prompt mode.
-                    KeyCode::Esc => match self.active_buffer {
-                        BufferType::Command => self.active_buffer = BufferType::Prompt,
-                        _ => (),
-                    },
+                    KeyCode::Esc => {
+                        if let TextBufferKind::Command = self.active_buffer_kind {
+                            self.active_buffer_kind = TextBufferKind::Prompt
+                        }
+                    }
                     // Enter: Submit prompt or command.
-                    KeyCode::Enter => match self.active_buffer {
-                        BufferType::Prompt => {
-                            if self.wait_resp || state.prompt_buffer_mut().is_empty() {
+                    KeyCode::Enter => match self.active_buffer_kind {
+                        TextBufferKind::Prompt => {
+                            if self.wait_response || state.prompt_buffer_mut().is_empty() {
                                 return;
                             }
 
-                            println!("placeholder");
+                            todo!();
                         }
-                        BufferType::Command => (),
+                        TextBufferKind::Command => (),
                     },
+                    KeyCode::Char(ch) => self.get_active_buffer(state).insert_char(ch),
+                    KeyCode::Delete => self.get_active_buffer(state).delete_char(),
+                    KeyCode::Backspace => self.get_active_buffer(state).delete_prev_char(),
+                    KeyCode::Left => self.get_active_buffer(state).move_cursor_left(),
+                    KeyCode::Right => self.get_active_buffer(state).move_cursor_right(),
                     _ => (),
                 },
                 _ => (),
             },
-            Event::Mouse(_) => {
-                println!("placeholder");
-            }
+            Event::Mouse(mouse) => match mouse.kind {
+                MouseEventKind::ScrollUp => {
+                    state.set_scroll_offset(state.scroll_offset().saturating_add(1));
+                }
+                MouseEventKind::ScrollDown => {
+                    state.set_scroll_offset(state.scroll_offset().saturating_sub(1));
+                }
+                _ => (),
+            },
             _ => (),
+        }
+    }
+
+    fn get_active_buffer<'a>(&self, state: &'a mut State) -> &'a mut TextBuffer {
+        match self.active_buffer_kind {
+            TextBufferKind::Prompt => state.prompt_buffer_mut(),
+            TextBufferKind::Command => state.command_buffer_mut(),
         }
     }
 }
